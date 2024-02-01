5f2b4ee5bfaf68cb20b6df5451baf8feb2c159ec
PatriPomes <pmanzanastomas@gmail.com>
2024-02-01 13:39:19 +0100

    Game Board variables

--- js/game.js
+++ js/game.js
@@ -8,6 +8,15 @@ var canvasY = 500;
 
 var tileX, tileY;
 
+//game board
+var gameBoard;
+var rows = 100;
+var columns = 100;
+
+var white = "#FFFFFF";
+var black = "#000000";
+
+
 function inicializa(){
     console.log('Carga');
 }
